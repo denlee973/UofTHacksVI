f884098ec404870289b4a56709aa1abbd39728bd
Denny <denlee973@users.noreply.github.com>
2019-01-19 10:39:06 -0500

    start!

---
+++ MakePrediction -- Tutorial.py
@@ -0,0 +1,53 @@
+# Install Custom Vision Service SDK
+# pip install azure-cognitiveservices-vision-customvision
+# Python SDK Samples https://github.com/Azure-Samples/cognitive-services-python-sdk-samples/tree/master/samples
+
+from azure.cognitiveservices.vision.customvision.training import CustomVisionTrainingClient
+# from azure.cognitiveservices.vision.customvision.training.models import ImageUrlCreateEntry
+from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
+# import time
+import os
+
+# Set these variables based on the settings of your trained project
+SAMPLE_PROJECT_NAME = "ElephantNoElephant"
+ENDPOINT = "https://southcentralus.api.cognitive.microsoft.com"
+training_endpoint = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.2/Training/"
+prediction_endpoint = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/"
+training_key = "6ea9a9db31724efd90b9bfe886ea108f"
+prediction_key = "4708b2d271e04f8a8e5453839f4e4921"
+
+
+#  function to fetch project object from Custom Vision Service
+def find_project():
+    try:
+        print("Get trainer")
+        trainer = CustomVisionTrainingClient(training_key, endpoint=ENDPOINT)
+
+        print("get project")
+        for proj in trainer.get_projects():
+            if (proj.name == SAMPLE_PROJECT_NAME):
+                return proj
+    except Exception as e:
+        print(str(e))
+
+
+print("Set path for folder containign image to predict")
+IMAGES_FOLDER = os.path.dirname(os.path.realpath(__file__))
+# print(str(IMAGES_FOLDER))
+# print(str(os.path.join(IMAGES_FOLDER, "TestImages", "testelephant2.jpg")))
+
+# Get predictor and project objects 
+predictor = CustomVisionPredictionClient(prediction_key, endpoint=ENDPOINT)
+project = find_project()
+
+print("Make prediction")
+try:
+    with open(os.path.join(IMAGES_FOLDER, "TestImages", "testgiraffe1.jpg"), mode="rb") as test_data:
+        results = predictor.predict_image(project.id, test_data.read())
+except Exception as e:
+    print(str(e))
+    input()
+# Display the results.
+for prediction in results.predictions:
+    print(prediction.tag_name + ": {0:.2f}%".format(prediction.probability * 100))
+    input()
